05828fa43b75e4cc2f4b89a38c93df42b49ad20c
andreisas01 <andreisas01@yahoo.com>
2026-03-30 11:41:00 +0300

    init threads

---
+++ Threads.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laborator
+{
+    public partial class Threads : Form
+    {
+        public Threads()
+        {
+            InitializeComponent();
+        }
+    }
+}
