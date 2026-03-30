1b0c13a61a0a506b37a3b82a23c84d89ae11da95
andreisas01 <andreisas01@yahoo.com>
2026-03-30 12:45:29 +0300

    threads

--- Threads.Designer.cs
+++ Threads.Designer.cs
@@ -29,11 +29,97 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progress_bar_1 = new System.Windows.Forms.ProgressBar();
+            this.control_1_button = new System.Windows.Forms.Button();
+            this.control_2_button = new System.Windows.Forms.Button();
+            this.control_3_button = new System.Windows.Forms.Button();
+            this.progress_bar_2 = new System.Windows.Forms.ProgressBar();
+            this.progress_bar_3 = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progress_bar_1
+            // 
+            this.progress_bar_1.Location = new System.Drawing.Point(269, 87);
+            this.progress_bar_1.Maximum = 1000000;
+            this.progress_bar_1.Name = "progress_bar_1";
+            this.progress_bar_1.Size = new System.Drawing.Size(469, 41);
+            this.progress_bar_1.Step = 1;
+            this.progress_bar_1.TabIndex = 0;
+            // 
+            // control_1_button
+            // 
+            this.control_1_button.Location = new System.Drawing.Point(97, 87);
+            this.control_1_button.Name = "control_1_button";
+            this.control_1_button.Size = new System.Drawing.Size(112, 41);
+            this.control_1_button.TabIndex = 1;
+            this.control_1_button.Text = "start";
+            this.control_1_button.UseVisualStyleBackColor = true;
+            this.control_1_button.Click += new System.EventHandler(this.control_1_button_Click);
+            // 
+            // control_2_button
+            // 
+            this.control_2_button.Location = new System.Drawing.Point(97, 172);
+            this.control_2_button.Name = "control_2_button";
+            this.control_2_button.Size = new System.Drawing.Size(112, 41);
+            this.control_2_button.TabIndex = 2;
+            this.control_2_button.Text = "start";
+            this.control_2_button.UseVisualStyleBackColor = true;
+            this.control_2_button.Click += new System.EventHandler(this.control_2_button_Click);
+            // 
+            // control_3_button
+            // 
+            this.control_3_button.Location = new System.Drawing.Point(97, 261);
+            this.control_3_button.Name = "control_3_button";
+            this.control_3_button.Size = new System.Drawing.Size(112, 41);
+            this.control_3_button.TabIndex = 3;
+            this.control_3_button.Text = "start";
+            this.control_3_button.UseVisualStyleBackColor = true;
+            this.control_3_button.Click += new System.EventHandler(this.control_3_button_Click);
+            // 
+            // progress_bar_2
+            // 
+            this.progress_bar_2.Location = new System.Drawing.Point(269, 172);
+            this.progress_bar_2.Maximum = 1000000;
+            this.progress_bar_2.Name = "progress_bar_2";
+            this.progress_bar_2.Size = new System.Drawing.Size(469, 41);
+            this.progress_bar_2.Step = 1;
+            this.progress_bar_2.TabIndex = 4;
+            // 
+            // progress_bar_3
+            // 
+            this.progress_bar_3.Location = new System.Drawing.Point(269, 261);
+            this.progress_bar_3.Maximum = 1000000;
+            this.progress_bar_3.Name = "progress_bar_3";
+            this.progress_bar_3.Size = new System.Drawing.Size(469, 41);
+            this.progress_bar_3.Step = 1;
+            this.progress_bar_3.TabIndex = 5;
+            // 
+            // Threads
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progress_bar_3);
+            this.Controls.Add(this.progress_bar_2);
+            this.Controls.Add(this.control_3_button);
+            this.Controls.Add(this.control_2_button);
+            this.Controls.Add(this.control_1_button);
+            this.Controls.Add(this.progress_bar_1);
+            this.Name = "Threads";
             this.Text = "Threads";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progress_bar_1;
+        private System.Windows.Forms.Button control_1_button;
+        private System.Windows.Forms.Button control_2_button;
+        private System.Windows.Forms.Button control_3_button;
+        private System.Windows.Forms.ProgressBar progress_bar_2;
+        private System.Windows.Forms.ProgressBar progress_bar_3;
+        private System.Windows.Forms.Timer timer;
     }
 }
--- Threads.cs
+++ Threads.cs
@@ -10,11 +10,138 @@ using System.Windows.Forms;
 
 namespace laborator
 {
+    public enum ThreadState
+    {
+        New,
+        Running,
+        Suspended
+    }
     public partial class Threads : Form
     {
+        private static ThreadState thread_1_state, thread_2_state, thread_3_state;
+        private static IntPtr h_thread_1, h_thread_2, h_thread_3;
+        private static uint thread_1_id, thread_2_id, thread_3_id;
         public Threads()
         {
             InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+            thread_1_state = thread_2_state = thread_3_state = ThreadState.New;
+        }
+
+        private uint increment_progress_bar_1(IntPtr lpParameter)
+        {
+            while (progress_bar_1.Value < progress_bar_1.Maximum)
+            {
+                progress_bar_1.Value += 1;
+            }
+
+            return 0;
+        }
+        private uint increment_progress_bar_2(IntPtr lpParameter)
+        {
+            while (progress_bar_2.Value < progress_bar_2.Maximum)
+            {
+                progress_bar_2.Value += 5;
+            }
+
+            return 0;
+        }
+        private uint increment_progress_bar_3(IntPtr lpParameter)
+        {
+            while (progress_bar_3.Value < progress_bar_3.Maximum)
+            {
+                progress_bar_3.Value += 10;
+            }
+
+            return 0;
+        }
+
+        private void control_1_button_Click(object sender, EventArgs e)
+        {
+            switch (thread_1_state)
+            {
+                case ThreadState.New:
+                    h_thread_1 = (IntPtr)WinApiClass.CreateThread(IntPtr.Zero, 0, increment_progress_bar_1,
+                                        IntPtr.Zero, 0, out thread_1_id);
+
+                    thread_1_state = ThreadState.Running;
+                    control_1_button.Text = "suspend";
+                    break;
+
+                case ThreadState.Running:
+                    WinApiClass.SuspendThread(h_thread_1);
+
+                    thread_1_state = ThreadState.Suspended;
+                    control_1_button.Text = "resume";
+
+                    break;
+
+                case ThreadState.Suspended:
+                    WinApiClass.ResumeThread(h_thread_1);
+
+                    thread_1_state = ThreadState.Running;
+                    control_1_button.Text = "suspend";
+                    break;
+            }
+        }
+
+        private void control_2_button_Click(object sender, EventArgs e)
+        {
+            switch (thread_2_state)
+            {
+                case ThreadState.New:
+                    h_thread_2 = (IntPtr)WinApiClass.CreateThread(IntPtr.Zero, 0, increment_progress_bar_2,
+                                        IntPtr.Zero, 0, out thread_2_id);
+
+                    thread_2_state = ThreadState.Running;
+                    control_2_button.Text = "suspend";
+                    break;
+
+                case ThreadState.Running:
+                    WinApiClass.SuspendThread(h_thread_2);
+
+                    thread_2_state = ThreadState.Suspended;
+                    control_2_button.Text = "resume";
+
+                    break;
+
+                case ThreadState.Suspended:
+                    WinApiClass.ResumeThread(h_thread_2);
+
+                    thread_2_state = ThreadState.Running;
+                    control_2_button.Text = "suspend";
+                    break;
+            }
+        }
+
+        private void control_3_button_Click(object sender, EventArgs e)
+        {
+            switch (thread_3_state)
+            {
+                case ThreadState.New:
+                    h_thread_3 = (IntPtr)WinApiClass.CreateThread(IntPtr.Zero, 0, increment_progress_bar_3,
+                                        IntPtr.Zero, 0, out thread_3_id);
+
+                    thread_3_state = ThreadState.Running;
+                    control_3_button.Text = "suspend";
+                    break;
+
+                case ThreadState.Running:
+                    WinApiClass.SuspendThread(h_thread_3);
+
+                    thread_3_state = ThreadState.Suspended;
+                    control_3_button.Text = "resume";
+
+                    break;
+
+                case ThreadState.Suspended:
+                    WinApiClass.ResumeThread(h_thread_3);
+
+                    thread_3_state = ThreadState.Running;
+                    control_3_button.Text = "suspend";
+                    break;
+            }
         }
     }
 }
